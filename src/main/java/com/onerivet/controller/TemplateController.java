749dac76a6d2039ced85af6b7444dabcf0c1acde
nisarg <nisargpatel947@gmail.com>
2026-04-12 23:29:43 +0530

    swagger

--- src/main/java/com/onerivet/controller/TemplateController.java
+++ src/main/java/com/onerivet/controller/TemplateController.java
@@ -14,37 +14,83 @@ import com.onerivet.dto.PlanResponseDto;
 import com.onerivet.dto.VehicleTypeResponseDto;
 import com.onerivet.service.TemplateService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
+@Tag(name = "Template APIs", description = "APIs for Insurance Template Flow")
 public class TemplateController {
 
     private final TemplateService service;
 
+
+    @Operation(summary = "Get all vehicle types")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Vehicle types fetched successfully"),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @GetMapping("/vehicle-types")
     public List<VehicleTypeResponseDto> getVehicleTypes() {
         return service.getVehicleTypes();
     }
 
+
+    @Operation(summary = "Get plans by vehicle type")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Plans fetched successfully"),
+        @ApiResponse(responseCode = "404", description = "Vehicle type not found")
+    })
     @GetMapping("/vehicle-types/{id}/plans")
-    public List<PlanResponseDto> getPlans(@PathVariable Integer id) {
+    public List<PlanResponseDto> getPlans(
+            @Parameter(description = "Vehicle Type ID", example = "1")
+            @PathVariable Integer id) {
+
         return service.getPlansByVehicleType(id);
     }
 
+    
+    @Operation(summary = "Get coverages by template plan")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Coverages fetched successfully"),
+        @ApiResponse(responseCode = "404", description = "Template plan not found")
+    })
     @GetMapping("/template-plans/{id}/coverages")
-    public List<CoverageResponseDto> getCoverages(@PathVariable Integer id) {
+    public List<CoverageResponseDto> getCoverages(
+            @Parameter(description = "Template Plan ID", example = "1")
+            @PathVariable Integer id) {
+
         return service.getCoverages(id);
     }
 
+    
+    @Operation(summary = "Get coverage types by coverage")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Coverage types fetched successfully")
+    })
     @GetMapping("/coverages/{id}/types")
-    public List<CoverageTypeResponseDto> getTypes(@PathVariable Integer id) {
+    public List<CoverageTypeResponseDto> getTypes(
+            @Parameter(description = "Coverage ID", example = "1")
+            @PathVariable Integer id) {
+
         return service.getTypes(id);
     }
 
+    
+    @Operation(summary = "Get components by coverage type")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Components fetched successfully")
+    })
     @GetMapping("/coverage-types/{id}/components")
-    public List<CoverageComponentResponseDto> getComponents(@PathVariable Integer id) {
+    public List<CoverageComponentResponseDto> getComponents(
+            @Parameter(description = "Coverage Type ID", example = "3")
+            @PathVariable Integer id) {
+
         return service.getComponents(id);
     }
 }
